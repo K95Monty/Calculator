3d9800d913dd0489a854b4ee905cddbba79e889f
K95Monty <48034846+K95Monty@users.noreply.github.com>
2021-10-08 17:29:33 +0100

    Add files via upload
    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const equation = "2+4";
+const equationSpilt = equation.split("");
+
+function stringToNum() {
+    equationSpilt.forEach(element =>)
+};
+
+
+function submitEquation () {
+    document.getElementById("output").innerHTML = "hello";
+}
