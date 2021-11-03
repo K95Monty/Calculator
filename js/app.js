8adccdef16150dda245bee6ac60b0fdb5564acb7
K95Monty <48034846+K95Monty@users.noreply.github.com>
2021-11-03 00:03:44 +0000

    Add files via upload
    used switch case for working out the equation and appending changes to display
    
    styled buttons in a grid system
    
    sorted out calculator's size on small widths - now responsive
    
    added and changed ids of numbers and operators for cleaner code
    
    removed footer

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
-const equation = "22 + 4";
-const equationSpilt = equation.split(" "); //spilt the eqution into an array
+let currentDisplay = document.getElementById("current-display");
+let prevDisplay = document.getElementById("prev-display");
+let buttons = Array.from(document.getElementsByClassName("buttons"));
 
-//check if the string elements can be turned into a number
-//push these into a new array
-//calculate the total in the array
-
-
-function submit () {
-    document.getElementById("output").innerHTML = "hello";
+//alternative to eval
+function equals(equation) {
+    return new Function('return ' + equation)();
 }
+  
+buttons.map( button => {
+    button.addEventListener("click", (e) => {
+        switch(e.target.innerText) {
+            case "AC":
+                currentDisplay.innerText = "";
+                prevDisplay.innerText = "";
+                break;
+            case "DEL":
+                if(currentDisplay.innerText) {
+                    currentDisplay.innerText = currentDisplay.innerText.slice(0, - 1);
+                }
+                break;
+            case ".":
+                if(!currentDisplay.innerText.includes(".")) {
+                    currentDisplay.innerText += "."
+                }
+                break;
+            case "=":
+                try {
+                    currentDisplay.innerText = equals(currentDisplay.innerText);
+                    prevDisplay.innerText = currentDisplay.innerText;
+                    prevDisplay.innerText = equals(currentDisplay.innerText);
+                }
+                catch(error) {
+                    currentDisplay.innerText = "Error";
+                }
+                break; 
+            default:
+                currentDisplay.innerText += e.target.innerText;
+                break;
+        }
+        // console.log("clicked");
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(e.target.innerText);
+    })
+})  
